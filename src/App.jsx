472261ab9b3545fbce3006d03250ce383b513e1f
Heath Scott <hsco117@wgu.edu>
2025-12-25 11:08:28 -0700

    Added modification color scheme and header descriptions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-function App() {
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
+import header_img from "/src/assets/headshot-smaller-no-background.png";
+const projectDescriptions = ['foundational', 'engaging', 'thoughtful'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+
+function Header() {
+    const description = projectDescriptions[genRandomInt(2)];
+    return (<header>
+        <img src={header_img} alt="Headshot"/>
+        <h1>Project Portfolio</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
+            Check out some of my {description} projects.
         </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    </header>);
+}
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <h2>Time to get started!</h2>
+            </main>
+        </div>
+    );
 }
 
 export default App;
